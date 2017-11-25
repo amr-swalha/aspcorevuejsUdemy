6f32d6ad1b7d020d3e9009f98646214d7be921f0
Amr Swalha <amr.swalha.code@outlook.com>
2017-11-25 14:57:05 +0200

    -First Example -> Interpolations

---
+++ ClientApp/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <h2>Student Info</h2>
+        Student Name:{{student.name}}<br/>
+        Student Age:{{student.age}}
+        <button @click="UpdateName()">update name</button>
+        <br/>
+        3+5:{{ 3 +5 }}
+        <br/>
+        3>5:{{ 3 >5 }}
+        <br />
+        <!--<span title="{{msg}}"> some text</span>-->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello component msg',
+            student: {
+                name: 'Amr',
+                age:26
+            }
+        }
+        },
+    methods: {
+        UpdateName: function () {
+            this.student.name = "new Name";
+        }
+    }
+}
+</script>
+
+<style>
+    h1{
+        color:red;
+    }
+</style>
--- ClientApp/routes.js
+++ ClientApp/routes.js
@@ -1,9 +1,11 @@
 import CounterExample from 'components/counter-example'
 import FetchData from 'components/fetch-data'
 import HomePage from 'components/home-page'
+import Hello from 'components/Hello'
 
 export const routes = [
     { path: '/', component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
     { path: '/counter', component: CounterExample, display: 'Counter', style: 'glyphicon glyphicon-education' },
-    { path: '/fetch-data', component: FetchData, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' }
+    { path: '/fetch-data', component: FetchData, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' },
+    { path: '/Hello', component: Hello, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' }
 ]
